8ed959575ede86e109d41b5271366fc87aec7bda
LuK1337 <priv.luk@gmail.com>
2019-10-02 12:11:00 +0200

    Fix sync_direct_message_buffers time comparison

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ def sync_direct_message_buffers():
 
     while threading.current_thread().is_alive:
         # Update direct message buffers every 15 seconds
-        if time.time() % 15.0 == 0:
+        if time.time() % 15.0 <= 0.05:
             buffers = relay_client.get_direct_message_buffers()
 
             if buffers is not None:
