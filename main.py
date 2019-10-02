3f3e075c6f181258edf630b1688a2f6e6d79a0ab
LuK1337 <priv.luk@gmail.com>
2019-10-02 17:38:26 +0200

    Add logging in on_buffer_opened(), on_buffer_closed()

--- main.py
+++ main.py
@@ -64,6 +64,8 @@ def on_buffer_opened(e):
     buffer_name = Utils.get_slack_direct_message_channel_for_buffer(e['full_name'])
 
     if buffer_name is not None and buffer_name not in slack_client.last_dm_channels:
+        logging.info("Adding DM channel: {}".format(buffer_name))
+
         slack_client.create_dm_channels(slack_client.last_dm_channels + [buffer_name])
 
 
@@ -73,6 +75,8 @@ def on_buffer_closing(e):
     buffer_name = Utils.get_slack_direct_message_channel_for_buffer(e['full_name'])
 
     if buffer_name is not None and buffer_name in slack_client.last_dm_channels:
+        logging.info("Closing DM channel: {}".format(buffer_name))
+
         slack_client.last_dm_channels.remove(buffer_name)
         slack_client.clean_up_dm_channels(slack_client.last_dm_channels)
 
