fe01d774b83e7c11c97a634375e652ef589555fa
LuK1337 <priv.luk@gmail.com>
2019-10-05 22:31:14 +0200

    Add more realistic Utils.weechat_string_remove_color() tests

--- test/test_weechat_string_remove_color.py
+++ test/test_weechat_string_remove_color.py
@@ -40,8 +40,23 @@ class TestWeeChatStringRemoveColor(unittest.TestCase):
         self.assertEqual(Utils.weechat_string_remove_color('\x1C'), '')
 
         self.assertEqual(Utils.weechat_string_remove_color(
-            'Mode \x1913#titandev-test \x1928[\x1c+q a!*@*\x1928]\x1c by \x1915LuK1337'),
-            'Mode #titandev-test [+q a!*@*] by LuK1337')
+            '\x19F12luk\x1928 (\x1927~luk@kremowka.xyz\x1928)\x19F05 has joined \x1913#titandev-test\x19F05'),
+            'luk (~luk@kremowka.xyz) has joined #titandev-test')
+        self.assertEqual(Utils.weechat_string_remove_color(
+            '\x19F12luk\x1928 (\x1927~luk@kremowka.xyz\x1928)\x19F03 has left \x1913#titandev-test\x19F03'),
+            'luk (~luk@kremowka.xyz) has left #titandev-test')
+        self.assertEqual(Utils.weechat_string_remove_color(
+            '\x19F12luk\x1928 (\x1927~luk@kremowka.xyz\x1928)\x19F03 has quit \x1928(\x19F00Client Quit\x1928)'),
+            'luk (~luk@kremowka.xyz) has quit (Client Quit)')
+        self.assertEqual(Utils.weechat_string_remove_color(
+            '\x1915LuK1337\x19F03 has kicked \x19F12luk\x19F03 \x1928(\x1cluk\x1928)'),
+            'LuK1337 has kicked luk (luk)')
+        self.assertEqual(Utils.weechat_string_remove_color(
+            '\x1915LuK1337\x1c has changed topic for \x1913#titandev-test\x1c from "hi\x1c" to "\x19F16hello\x1c"'),
+            'LuK1337 has changed topic for #titandev-test from "hi" to "hello"')
+        self.assertEqual(Utils.weechat_string_remove_color(
+            'Mode \x1913#titandev-test \x1928[\x1c+r\x1928]\x1c by \x1915LuK1337'),
+            'Mode #titandev-test [+r] by LuK1337')
 
 
 if __name__ == '__main__':
